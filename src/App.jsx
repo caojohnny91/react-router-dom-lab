1ebf1d70321e4e10bef132775657536b69a56034
jcao91 <jcao91@yahoo.com>
2024-07-02 12:28:27 -0500

    addMox function created and initialState

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,37 @@ import MailboxDetails from "./components/MailboxDetails";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 
+const initialState = {
+  _id: 1,
+  boxSize: "Small",
+  boxholder: "Alex",
+};
 function App() {
+  const [mailboxes, setMailboxes] = useState([initialState]);
+
+  const addBox = (newMailboxData) => {
+    newMailboxData._Id = mailboxes.length + 1;
+    setMailboxes(...mailboxes, newMailboxData);
+  };
+
   return (
     <>
       <NavBar />
       <Routes>
-
-        <Route path="/" element={<main><h1>Post Office</h1></main>} />
+        <Route
+          path="/"
+          element={
+            <main>
+              <h1>Post Office</h1>
+            </main>
+          }
+        />
 
         <Route path="/mailboxes" element={<MailboxList />} />
 
         <Route path="/new-mailboxes" element={<MailboxForm />} />
 
         <Route path="/mailboxes/:mailboxId" element={<MailboxDetails />} />
-
-
       </Routes>
     </>
   );
