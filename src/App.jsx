21c45bab9a3ced3a06d171ff045d0284e4f695e4
jcao91 <jcao91@yahoo.com>
2024-07-02 11:49:43 -0500

    NavBar.jsx created with links

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import NavBar from "./components/NavBar";
+// import MailboxForm from "./components/MailboxForm";
+// import MailboxList from "./components/MailboxList";
+// import MailboxDetails from "./components/MailboxDetails";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
 
 function App() {
-
   return (
     <>
-    <h1>hello?</h1>
+      <NavBar />
+      <h1>hello?</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
